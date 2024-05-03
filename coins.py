a6bdfc18b7380f2d6753f922a36860765bef9f50
FoxBad <66217650+FoxBad@users.noreply.github.com>
2024-05-03 11:59:41 +0200

    Update coins.py

--- coins.py
+++ coins.py
@@ -1,1 +1,16 @@
 
+import random, os, pygame
+
+coinsimg = pygame.image.load(os.path.join("assets", "coins.png"))
+coinsimg = pygame.transform.scale(coinsimg, (50, 50))
+
+class CoinsC(pygame.sprite.Sprite):
+    def __init__(self, groups, ws, hs):
+        super().__init__(groups)
+        self.x = random.randint(0, ws)
+        self.y = random.randint(0, hs)
+        self.image = coinsimg
+        self.rect = self.image.get_rect(center = (self.x,self.y))
+    
+
+
