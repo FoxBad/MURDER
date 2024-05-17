4bbcc70b8f667acbaaca7a5e6814bab05fec8ce8
FoxBad <66217650+FoxBad@users.noreply.github.com>
2024-05-17 11:55:54 +0200

    Update redcross.py

--- redcross.py
+++ redcross.py
@@ -4,8 +4,9 @@ redcross = pygame.image.load(os.path.join("assets", "redcross.png"))
 redcross = pygame.transform.scale(redcross, (60, 60))
 
 class RedCross(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
-        super().__init__(groups)
+    def __init__(self, pos, allspritegroup, groups):
+        super().__init__(allspritegroup)
+        groups.add(self)
         self.pos = pos
         self.image = redcross
         self.rect = self.image.get_rect(center = self.pos)
